962081b56e2b6aed964c7cd8b6f52f4eca016e32
Sarah Thomson <sthom120@gmail.com>
2025-12-15 17:35:16 +1000

    change pool

--- db/pool.js
+++ db/pool.js
@@ -10,11 +10,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Create pool
+const mysql = require("mysql2/promise");
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST || process.env.DB_HOST || "localhost",
+  port: Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306),
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = { pool };
+
